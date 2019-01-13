ff121c286c481c30f1edb7ff32f38537ba63cf6d
Andranique Boone <andranique.boone@gmail.com>
2019-01-13 04:44:15 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -3,13 +3,7 @@ class Person
     @name = name
     @job = job
   end
-  def name
-    @name
-  end
-  def job=(job)
-    @bjob = job
-  end
 end
 
-person = Person.new("Andranique")
-person.job= "future programer"
+Person.new("Andranique")
+
