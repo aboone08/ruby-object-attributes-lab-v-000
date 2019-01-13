e19af65d9209cfe475156ecf1cfb3d510314d386
Andranique Boone <andranique.boone@gmail.com>
2019-01-13 03:56:30 +0000

    Automatically backed up by Learn

---
+++ lib/person.rb
@@ -0,0 +1,15 @@
+class Person
+  def initialize (name, job)
+    @name = Andranique
+    @job = future_developer
+  end
+  def Andranique
+    @name
+  end
+  def future_developer
+    @job
+  end
+end
+
+Person.name
+Person.job
