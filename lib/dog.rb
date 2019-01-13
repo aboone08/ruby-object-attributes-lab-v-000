10609e62229f050a70b772e66cf552f687247c22
Andranique Boone <andranique.boone@gmail.com>
2019-01-13 04:42:40 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -3,16 +3,6 @@ class Dog
     @name = name
     @breed = breed
   end
-  def name= (name, breed = "morkie")
-    @name = name
-    @breed = breed
-  end 
-  def name 
-    @name
-  end
-  def breed 
-    @breed 
-  end 
 end
 
 kingsley = Dog.new
